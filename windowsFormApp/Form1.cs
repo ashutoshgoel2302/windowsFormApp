58bb989df601fb88156690295e927647bb39c916
ashutoshgoel2302 <43414593+ashutoshgoel2302@users.noreply.github.com>
2018-10-15 18:38:33 +0530

    Initial commit

---
+++ windowsFormApp/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windowsFormApp
+{
+    public partial class Form1 : Form
+    {
+        AngularEntities angularEntities = new AngularEntities();
+       
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Text = string.IsNullOrEmpty(Form2.SetValueForText1) ?"": Form2.SetValueForText1;
+            textBox2.Text = string.IsNullOrEmpty(Form2.SetValueForText2) ? "" : Form2.SetValueForText2;
+            comboBox1.Text = string.IsNullOrEmpty(Form2.SetValueForText3) ? "" : Form2.SetValueForText3;
+            textBox4.Text = string.IsNullOrEmpty(Form2.SetValueForText4) ? "" : Form2.SetValueForText4;
+            textBox5.Text = string.IsNullOrEmpty(Form2.SetValueForText5) ? "" : Form2.SetValueForText5;
+            comboBox2.Text = string.IsNullOrEmpty(Form2.SetValueForText6) ? "" : Form2.SetValueForText6;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          //  MessageBox.Show("hi editpage");
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Child children = new Child()
+            {
+                FirstName = textBox1.Text,
+                LastName=textBox2.Text,
+                Gender=comboBox1.Text,
+                Address=textBox4.Text,
+                BirthDate=Convert.ToDateTime(textBox5.Text),
+                ChildType=comboBox2.Text,
+            };
+            angularEntities.Children.Add(children);
+            angularEntities.SaveChanges();
+            clearText();
+        }
+
+        public void clearText()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            comboBox1.Text = "";
+            comboBox2.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var childId = Convert.ToInt32(Form2.SetValueID);
+            if(!string.IsNullOrEmpty(Form2.SetValueID))
+            {
+               var editableChild= angularEntities.Children.Where(x => x.ID == childId).FirstOrDefault();
+                editableChild.FirstName = textBox1.Text;
+                editableChild.LastName = textBox2.Text;
+                editableChild.Gender = comboBox1.Text;
+                editableChild.Address = textBox4.Text;
+                editableChild.BirthDate = Convert.ToDateTime(textBox5.Text);
+                editableChild.ChildType = comboBox2.Text;
+
+                angularEntities.SaveChanges();
+                clearText();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var childId = Convert.ToInt32(Form2.SetValueID);
+            if (!string.IsNullOrEmpty(Form2.SetValueID))
+            {
+                var editableChild = angularEntities.Children.Where(x => x.ID == childId).FirstOrDefault();
+                angularEntities.Children.Remove(editableChild);
+
+                angularEntities.SaveChanges();
+                clearText();
+            }
+        }
+    }
+}
---
+++ windowsFormApp/Form2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windowsFormApp
+{
+    public partial class Form2 : Form
+    {
+        public static string SetValueForText1 = "";
+        public static string SetValueForText2 = "";
+        public static string SetValueForText3 = "";
+        public static string SetValueForText4 = "";
+        public static string SetValueForText5 = "";
+        public static string SetValueForText6 = "";
+        public static string SetValueID = "";
+
+        AngularEntities angularEntities = new AngularEntities();
+        public Form2()
+        {
+            InitializeComponent();
+           
+            //dataGridView1.DataSource = (from data in angularEntities.Children
+            //                            select data).ToList();
+
+            dataGridView1.DataSource = angularEntities.Children.ToList();
+        }
+       
+
+     
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(textBox1.Text))
+            {
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = angularEntities.Children.Where(x => x.FirstName == textBox1.Text).ToList();
+            }
+            if (!String.IsNullOrEmpty(textBox2.Text))
+            {
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = angularEntities.Children.Where(x => x.LastName == textBox2.Text).ToList();
+            }
+            if ((!String.IsNullOrEmpty(textBox1.Text) && !String.IsNullOrEmpty(textBox2.Text)))
+            {
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = angularEntities.Children.Where(x => x.FirstName == textBox1.Text && x.LastName == textBox2.Text).ToList();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            dataGridView1.DataSource = angularEntities.Children.ToList();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dataGridView1.SelectedRows) // foreach datagridview selected rows values  
+                {
+                    SetValueID = row.Cells[0].Value.ToString();
+                    SetValueForText1 = row.Cells[1].Value.ToString();
+                    SetValueForText2 = row.Cells[2].Value.ToString();
+                    SetValueForText3 = row.Cells[3].Value.ToString();
+                    SetValueForText4 = row.Cells[4].Value.ToString();
+                    SetValueForText5 = row.Cells[5].Value.ToString();
+                    SetValueForText6 = row.Cells[6].Value.ToString();
+                }
+                Form1 form1 = new Form1();
+                form1.Show();
+            }
+        }
+    }
+}
